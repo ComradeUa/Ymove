e91feb05f90f0b67ed079003fce9c285e7618522
ComradeUa <az63.zov@gmail.com>
2025-09-11 23:02:28 +0300

    feat: add TrendingSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import SearchBar from '@/components/SearchBar';
+import TrendingSection from '@/components/TredningSections';
 
 export default function Home() {
   return (
     <>
       <main>
         <SearchBar />
+        <TrendingSection />
       </main>
     </>
   );
