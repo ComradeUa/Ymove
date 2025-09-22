65b6c62fdecfa6994a0f8a2a57c35afeacc2958a
ComradeUa <az63.zov@gmail.com>
2025-09-22 08:37:59 +0300

    feat: add MovieTrailer component

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,11 +1,13 @@
+'use client';
 import MovieDetailsCard from '@/components/MovieDetailsCard';
+import MovieTrailer from '@/components/MovieTrailer';
 import TopBilledCast from '@/components/TopBilledCast';
-
 export default function MoviePage() {
   return (
     <div>
       <MovieDetailsCard />
       <TopBilledCast />
+      <MovieTrailer />
     </div>
   );
 }
