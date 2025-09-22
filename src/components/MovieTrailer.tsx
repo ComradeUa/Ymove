59f73807c2c0059ee3c53d612fe3749ee3f33728
ComradeUa <az63.zov@gmail.com>
2025-09-22 08:46:32 +0300

    feat: add new components MovieTrailer and its implementation

---
+++ src/components/MovieTrailer.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { useParams } from 'next/navigation';
+import { useVideo } from '@/hooks/useVideo';
+import ReactPlayer from 'react-player';
+const MovieTrailer: FC = () => {
+  const params = useParams();
+  const { id } = params;
+
+  const { trailer, loading, error } = useVideo(Number(id));
+
+  if (loading) return <div>Загрузка трейлера...</div>;
+  if (error) return <div>Ошибка: {error}</div>;
+  return (
+    <div className="flex flex-col items-center w-full h-[50rem]">
+      <h1 className="text-center text-3xl font-bold mb-4">Trailer</h1>
+      <div className="w-full max-w-[50%] aspect-video">
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${trailer?.key}`}
+          title={trailer?.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MovieTrailer;
