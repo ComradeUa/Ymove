075c6bef96e04d24274bcb47610855ec0c538d54
ComradeUa <az63.zov@gmail.com>
2025-09-22 08:40:12 +0300

    feat: update MovieDetailsCard layout and prepare for TopBilledCast

--- src/components/MovieDetailsCard.tsx
+++ src/components/MovieDetailsCard.tsx
@@ -5,7 +5,7 @@ import { useMovieById } from '@/hooks/useMovieById';
 import Image from 'next/image';
 import MoviePoster from './MoviePoster';
 import MovieInfo from './MovieInfo';
-
+import TopBilledCast from './TopBilledCast';
 const MovieDetailsCard: FC = () => {
   const params = useParams();
   const { id } = params;
@@ -16,33 +16,35 @@ const MovieDetailsCard: FC = () => {
     return <p className="text-red-500 text-center mt-10">{error ?? 'Movie not found'}</p>;
 
   return (
-    <div className="relative w-full">
-      {/* Фон */}
-      {movie.backdrop_path && (
-        <Image
-          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-          alt={movie.title || 'Background'}
-          fill
-          className="object-cover"
-          priority
-        />
-      )}
-      <div className="absolute inset-0 bg-black/80" />
+    <div>
+      <div className="relative w-full">
+        {/* Фон */}
+        {movie.backdrop_path && (
+          <Image
+            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+            alt={movie.title || 'Background'}
+            fill
+            className="object-cover"
+            priority
+          />
+        )}
+        <div className="absolute inset-0 bg-black/80" />
 
-      {/* Контент */}
-      <div className="relative z-10 flex items-center gap-8 p-10">
-        {/* Постер */}
-        <MoviePoster posterPath={movie.poster_path} title={movie.title} />
+        {/* Контент */}
+        <div className="relative z-10 flex items-center gap-9 p-10  justify-center">
+          {/* Постер */}
+          <MoviePoster posterPath={movie.poster_path} title={movie.title} />
 
-        {/* Текст */}
-        <MovieInfo
-          title={movie.title}
-          releaseDate={movie.release_date}
-          genres={movie.genres}
-          runtime={movie.runtime}
-          overview={movie.overview}
-          voteAverage={movie.vote_average}
-        />
+          {/* Текст */}
+          <MovieInfo
+            title={movie.title}
+            releaseDate={movie.release_date}
+            genres={movie.genres}
+            runtime={movie.runtime}
+            overview={movie.overview}
+            voteAverage={movie.vote_average}
+          />
+        </div>
       </div>
     </div>
   );
