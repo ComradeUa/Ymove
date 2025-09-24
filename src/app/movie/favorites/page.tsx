9d02f37e9282cb535f5b8012447f6a8a4d0497d2
ComradeUa <az63.zov@gmail.com>
2025-09-24 03:51:54 +0300

    refactor: remove extra code

--- src/app/movie/favorites/page.tsx
+++ src/app/movie/favorites/page.tsx
@@ -1,17 +1,10 @@
 'use client';
 
-import {
-  useAddFavoriteMutation,
-  useDeleteFavoriteMutation,
-  useGetFavoritesQuery,
-} from '@/store/api/favorite';
+import { useGetFavoritesQuery } from '@/store/api/favorite';
 import Image from 'next/image';
-import { useDispatch, useSelector } from 'react-redux';
 
 export default function FavoritesPage() {
   const { data: favorites, isLoading } = useGetFavoritesQuery();
-  const [addFavorite] = useAddFavoriteMutation();
-  const [deleteFavorite] = useDeleteFavoriteMutation();
   if (isLoading) return <p>Загрузка...</p>;
   return (
     <>
