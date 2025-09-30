c1c4c8c11a705c14a38e52b417fb367b64e5846b
ComradeUa <az63.zov@gmail.com>
2025-09-30 12:33:19 +0300

    feat: implemented useToggleFavorite for adding and removing favorites

--- src/hooks/useToggleFavorite.ts
+++ src/hooks/useToggleFavorite.ts
@@ -1,40 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react';
 import {
   useGetFavoritesQuery,
   useAddFavoriteMutation,
   useDeleteFavoriteMutation,
-} from '@/store/api/favorite'
+} from '@/store/api/favorite';
 
-export const useToggleFavorite = (movie_id:number) => {
-    const { data: favorites } = useGetFavoritesQuery()
-  const [isFavorite, setIsFavorite] = useState<boolean>(false)
-  const [addFavorite] = useAddFavoriteMutation()
-  const [deleteFavorite] = useDeleteFavoriteMutation()
+export const useToggleFavorite = (movie_id: number) => {
+  const { data: favorites } = useGetFavoritesQuery();
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [addFavorite] = useAddFavoriteMutation();
+  const [deleteFavorite] = useDeleteFavoriteMutation();
 
   useEffect(() => {
     if (favorites) {
-      const found = favorites.some((f) => f.id === movie_id)
-      setIsFavorite(found)
+      const found = favorites.some((f) => f.id === movie_id);
+      setIsFavorite(found);
     }
-  }, [favorites, movie_id])
+  }, [favorites, movie_id]);
 
-  const toggleFavoriteClick = async () => {
-    setIsFavorite((prev) => !prev) 
+  const toggleFavoriteClick = useCallback(async () => {
+    const nextState = !isFavorite;
+    setIsFavorite(nextState);
 
     try {
-      if (!isFavorite) {
-        await addFavorite({ movie_id }).unwrap()
+      if (nextState) {
+        await addFavorite({ movie_id }).unwrap();
       } else {
-        const favorite = favorites?.find((f) => f.id === movie_id)
+        const favorite = favorites?.find((f) => f.id === movie_id);
         if (favorite) {
-          await deleteFavorite(favorite.id).unwrap()
+          await deleteFavorite(favorite.id).unwrap();
         }
       }
     } catch (e) {
-      console.error('Ошибка при переключении избранного', e)
-      setIsFavorite((prev) => !prev) 
+      console.error('Error ', e);
+      setIsFavorite((prev) => !prev); 
     }
-  }
+  }, [isFavorite, favorites, addFavorite, deleteFavorite, movie_id]);
 
-  return { isFavorite, toggleFavoriteClick }
-}
+  return { isFavorite, toggleFavoriteClick };
+};
