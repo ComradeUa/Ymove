1f3b099bcc695858ba9b9e82c4272d2c0a04fa4e
ComradeUa <az63.zov@gmail.com>
2025-10-02 12:12:35 +0300

    feat: add user-specific favorites API and client integration

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -2,12 +2,22 @@
 import { useGetFavoritesQuery } from '@/store/api/favorite';
 import React, { type FC } from 'react';
 import FavoriteItem from './FavoriteItem';
+import { useSession } from '@/lib/auth-client';
+import { skipToken } from '@reduxjs/toolkit/query';
 const FavoritesList: FC = () => {
-  const { data: favorites, isLoading, isError } = useGetFavoritesQuery();
+  const { data: session, isPending, error } = useSession();
+  const user_id = session?.user?.id; // строка или undefined
 
+  const {
+    data: favorites,
+    isLoading,
+    isError,
+  } = useGetFavoritesQuery(user_id ? { user_id } : skipToken);
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error loading favorites </p>;
+  if (!session) return <p>if you want to use the function then you have to sign in!</p>;
   if (!favorites || favorites.length === 0) {
+    // eslint-disable-next-line react/no-unescaped-entities
     return <p>You don't have any favorites</p>;
   }
   return (
