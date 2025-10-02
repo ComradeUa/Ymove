45029cc554404ff391e3c833018674d8ac27172b
ComradeUa <az63.zov@gmail.com>
2025-10-02 12:09:53 +0300

    feat: add page for sign-up

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from '@/components/SignUp';
+
+export default function SignUpPage() {
+  return (
+    <main className="flex justify-center items-center">
+      <SignUp />
+    </main>
+  );
+}
