9dba4fa963ec7fa08219e1242f1ddf5ad32edd80
ComradeUa <az63.zov@gmail.com>
2025-10-02 12:11:24 +0300

    feat: add user_id support in API and client delete mutation"

--- src/components/FavoriteItem.tsx
+++ src/components/FavoriteItem.tsx
@@ -6,15 +6,19 @@ import { Heart } from 'lucide-react';
 import { useDeleteFavoriteMutation } from '@/store/api/favorite';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 import MovieSearchCard from './MoviesSearchCard';
+import { useSession } from '@/lib/auth-client';
 type FavoriteItemProps = {
   movie: MediaItem;
 };
 
 const FavoriteItem: FC<FavoriteItemProps> = ({ movie }) => {
+  const { data: session } = useSession();
+  const user_id = session?.user?.id;
   const [deleteFavorite] = useDeleteFavoriteMutation();
 
   const handleRemove = () => {
-    deleteFavorite(movie.id);
+    if (!user_id) return;
+    deleteFavorite({ movie_id: movie.id, user_id });
   };
   return (
     <div className="flex flex-col gap-4 items-center mt-5 max-w-full">
