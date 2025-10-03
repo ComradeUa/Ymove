752243e2f473be9c823d742a3cc173900676da48
ComradeUa <az63.zov@gmail.com>
2025-10-03 08:39:38 +0300

    feat: add page for forgot-password

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetPassword from '@/components/ForgotPassword';
+
+export default function ForgotPasswordPage() {
+  return (
+    <main className="flex justify-center">
+      <ResetPassword />
+    </main>
+  );
+}
